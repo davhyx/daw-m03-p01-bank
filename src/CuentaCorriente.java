66a332f64b1839be94ef207eef3b01bdc8b9e76e
victor199710 <victormarchante1017@gmail.com>
2016-11-18 19:32:04 +0100

    newcc

--- src/CuentaCorriente.java
+++ src/CuentaCorriente.java
@@ -5,7 +5,25 @@ public class CuentaCorriente extends Cuenta implements Corriente {
         super(numCuenta, nif, saldo);
     }
 
+    public void setDeposito(int deposito){
+        int saldoTemp = getSaldo();
+        saldoTemp += deposito;
+        setSaldo(saldoTemp);
+    }
+
+    public void setExtraccion(int extraccion){
+        int saldoTemp = getSaldo();
+        saldoTemp -= extraccion;
+        setSaldo(saldoTemp);
+    }
+
+    @Override
+    public String toString(){
+        return "El saldo en la cuenta es: " + this.getSaldo();
+    }
+
     public CuentaCorriente(int saldo){
        super(saldo);
    }
+}
 }
